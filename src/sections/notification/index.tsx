e44854c4a454032b509776dbf98eb1b351625a0d
Mayur-R-code <mayurrathod1176@gmail.com>
2024-10-26 12:24:14 +0530

    memorized the code and remove a unuse code and add and clear all button in blog view and user table

---
+++ src/sections/notification/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Box } from '@mui/material';
+
+const NotificationsView = () => (
+        <Box>
+            NotificationsViews
+        </Box>
+
+    )
+export default NotificationsView
